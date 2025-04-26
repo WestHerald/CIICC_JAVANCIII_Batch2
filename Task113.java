d3aa5c9b945cbd47e7328752393d36f4ad9e8ca7
WestHerald <lewisdeleon143@gmail.com>
2025-04-26 15:58:49 +0800

    Task111, 112, and 113 - Done

---
+++ Task113.java
@@ -0,0 +1,10 @@
+public class Task113 {
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
+        for (int i = 1; i < check_number+1; i++) {
+            message = (i%2==0) ? i+ " is even number" : i+ " is odd number";
+            System.out.println(message);
+        }
+    }
+}
