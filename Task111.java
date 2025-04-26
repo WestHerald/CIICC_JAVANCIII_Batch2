314d7116484a908b15f60bc796e7fc601b06732f
WestHerald <lewisdeleon143@gmail.com>
2025-04-26 14:18:09 +0800

    Task111 - Hello World

--- Task111.java
+++ Task111.java
@@ -1,6 +1,9 @@
-public class Task111 {
-
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class helloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Kamusta Mundo!"); // Display the string.
     }
 }
