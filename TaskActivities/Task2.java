3899d7e51f08e5afcd1aefb7b2f3c49510fca60c
WestHerald <lewisdeleon143@gmail.com>
2025-05-02 15:23:31 +0800

    5/2 Activities

---
+++ TaskActivities/Task2.java
@@ -0,0 +1,12 @@
+public class Task2 {
+    public static void main(String[] args) {
+        byte zero = 0,
+        one = 1,
+        two = 2,
+        three = 3;
+        boolean yes = true;
+        String output = "H"+three+one+one+zero+" w"+zero+"r"+one+"d "+two+"."+zero+" "+yes;
+
+        System.out.println(output);
+    }
+}
